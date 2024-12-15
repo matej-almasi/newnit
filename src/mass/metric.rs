d60b0efc264430e53f3ae0bf75ecbd99ff0d8548
Matej Almasi <almasi.mato@gmail.com>
2024-12-15 15:52:46 +0100

    Expand SI units of mass

--- src/mass/metric.rs
+++ src/mass/metric.rs
@@ -9,6 +9,9 @@ use super::Mass;
 use crate::unit;
 use crate::Unit;
 
+unit!(Quettagram, 1E+27, Mass);
+unit!(Ronnagram, 1E+24, Mass);
+unit!(Yottagram, 1E+21, Mass);
 unit!(Zettagram, 1E+18, Mass);
 unit!(Exagram, 1E+15, Mass);
 unit!(Petagram, 1E+12, Mass);
@@ -23,6 +26,10 @@ unit!(Nanogram, 1E-12, Mass);
 unit!(Picogram, 1E-15, Mass);
 unit!(Femtogram, 1E-18, Mass);
 unit!(Attogram, 1E-21, Mass);
+unit!(Zeptogram, 1E-24, Mass);
+unit!(Yoctogram, 1E-27, Mass);
+unit!(Rontogram, 1E-30, Mass);
+unit!(Quectogram, 1E-33, Mass);
 
 /// Metric Tonne
 pub type Tonne = Megagram;
