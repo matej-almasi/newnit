1c28e7b81af48f3ad0e110a0a6fe43090ee75551
Matej Almasi <almasi.matej@protonmail.com>
2025-02-23 10:59:18 +0100

    implement division with length and area for Volume

--- src/volume.rs
+++ src/volume.rs
@@ -6,8 +6,24 @@
 //! - [`imperial`] - British Imperial units
 //! - [`metric`] - International System of Units (SI)
 
+use crate::Unit;
+use crate::area::Area;
+use crate::area::metric::SquareMeter;
+use crate::length::Length;
+use crate::length::metric::Meter;
+
 pub mod customary;
 pub mod imperial;
 pub mod metric;
 
-pub trait Volume {}
+pub trait Volume: Unit {
+    /// Divide a unit of volume by a unit of length
+    fn divide_length(&self, rhs: &dyn Length) -> SquareMeter {
+        SquareMeter(self.to_base() / rhs.to_base())
+    }
+
+    /// Divide a unit of volume by a unit of area
+    fn divide_area(&self, rhs: &dyn Area) -> Meter {
+        Meter(self.to_base() / rhs.to_base())
+    }
+}
