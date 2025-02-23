6db3288f4d6787837697b5468ca9c9b206b0327b
Matej Almasi <almasi.matej@protonmail.com>
2025-02-23 10:47:51 +0100

    add multiplication and division with Length for Area

--- src/area.rs
+++ src/area.rs
@@ -6,8 +6,21 @@
 //! - [`metric`] - International System of Units (SI)
 
 use crate::Unit;
+use crate::length::Length;
+use crate::length::metric::Meter;
+use crate::volume::metric::CubicMeter;
 
 pub mod imperial;
 pub mod metric;
 
-pub trait Area: Unit {}
+pub trait Area: Unit {
+    /// Multiply a unit of area with a unit of length
+    fn multiply_length(&self, rhs: &dyn Length) -> CubicMeter {
+        CubicMeter(self.to_base() * rhs.to_base())
+    }
+
+    /// Divide a unit of area by a unit of length.
+    fn divide_length(&self, rhs: &dyn Length) -> Meter {
+        Meter(self.to_base() / rhs.to_base())
+    }
+}
