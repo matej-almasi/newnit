96069e27eeba3aadd11a9029013923bd4febe7ae
Matej Almasi <almasi.mato@gmail.com>
2025-02-25 21:24:24 +0100

    uncomment commented blocks in Length trait module

--- newnit/src/length.rs
+++ newnit/src/length.rs
@@ -42,64 +42,64 @@ pub mod nautical;
 /// assert!((area.value() - 2.4384).abs() < 1e-5);
 /// ```
 pub trait Length: Unit {
-    // /// Multiply two units of length.
-    // fn multiply(&self, rhs: &dyn Length) -> SquareMeter {
-    //     SquareMeter(self.to_base() * rhs.to_base())
-    // }
+    /// Multiply two units of length.
+    fn multiply(&self, rhs: &dyn Length) -> SquareMeter {
+        SquareMeter(self.to_base() * rhs.to_base())
+    }
 
-    // /// Multiply a unit of length with a unit of area.
-    // fn multiply_area(&self, rhs: &dyn Area) -> CubicMeter {
-    //     CubicMeter(self.to_base() * rhs.to_base())
-    // }
+    /// Multiply a unit of length with a unit of area.
+    fn multiply_area(&self, rhs: &dyn Area) -> CubicMeter {
+        CubicMeter(self.to_base() * rhs.to_base())
+    }
 
-    // /// Divide a unit of length by a unit of time.
-    // fn divide_time(&self, rhs: &dyn Time) -> MeterPerSecond {
-    //     MeterPerSecond(self.to_base() / rhs.to_base())
-    // }
+    /// Divide a unit of length by a unit of time.
+    fn divide_time(&self, rhs: &dyn Time) -> MeterPerSecond {
+        MeterPerSecond(self.to_base() / rhs.to_base())
+    }
 
-    // /// Divide a unit of length by a unit of velocity.
-    // fn divide_velocity(&self, rhs: &dyn Velocity) -> Second {
-    //     Second(self.to_base() / rhs.to_base())
-    // }
+    /// Divide a unit of length by a unit of velocity.
+    fn divide_velocity(&self, rhs: &dyn Velocity) -> Second {
+        Second(self.to_base() / rhs.to_base())
+    }
 }
 
-// #[cfg(test)]
-// mod test {
-//     use super::*;
+#[cfg(test)]
+mod test {
+    use super::*;
 
-//     #[test]
-//     fn multiply_with_length() {
-//         let length1 = metric::Meter(2.0);
-//         let length2 = imperial::Foot(3.0);
+    #[test]
+    fn multiply_with_length() {
+        let length1 = metric::Meter(2.0);
+        let length2 = imperial::Foot(3.0);
 
-//         let area = length1.multiply(&length2);
-//         assert!((area.value() - 1.8288).abs() < 1e-5);
-//     }
+        let area = length1.multiply(&length2);
+        assert!((area.value() - 1.8288).abs() < 1e-5);
+    }
 
-//     #[test]
-//     fn multiply_with_area() {
-//         let length = metric::Meter(2.0);
-//         let area = crate::area::imperial::SquareInch(2.0);
+    #[test]
+    fn multiply_with_area() {
+        let length = metric::Meter(2.0);
+        let area = crate::area::imperial::SquareInch(2.0);
 
-//         let volume = length.multiply_area(&area);
-//         assert!((volume.value() - 2.58064e-3).abs() < 1e-9);
-//     }
+        let volume = length.multiply_area(&area);
+        assert!((volume.value() - 2.58064e-3).abs() < 1e-9);
+    }
 
-//     #[test]
-//     fn divide_by_time() {
-//         let length = metric::Meter(2.0);
-//         let time = crate::time::metric::Second(3.0);
+    #[test]
+    fn divide_by_time() {
+        let length = metric::Meter(2.0);
+        let time = crate::time::metric::Second(3.0);
 
-//         let velocity = length.divide_time(&time);
-//         assert!((velocity.value() - 0.66667).abs() < 1e-5);
-//     }
+        let velocity = length.divide_time(&time);
+        assert!((velocity.value() - 0.66667).abs() < 1e-5);
+    }
 
-//     #[test]
-//     fn divide_by_velocity() {
-//         let length = metric::Meter(2.0);
-//         let velocity = crate::velocity::metric::MeterPerSecond(3.0);
+    #[test]
+    fn divide_by_velocity() {
+        let length = metric::Meter(2.0);
+        let velocity = crate::velocity::metric::MeterPerSecond(3.0);
 
-//         let time = length.divide_velocity(&velocity);
-//         assert!((time.value() - 0.66667).abs() < 1e-5);
-//     }
-// }
+        let time = length.divide_velocity(&velocity);
+        assert!((time.value() - 0.66667).abs() < 1e-5);
+    }
+}
